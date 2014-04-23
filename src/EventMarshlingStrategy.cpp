ad4e04441239620f337df0b19fd992fec79e1761
nbroeking <nbroeking@me.com>
2014-04-23 11:31:31 -0600

    Added EventMarshlingStrategy

---
+++ src/EventMarshlingStrategy.cpp
@@ -0,0 +1,23 @@
+//
+//  EventMarshlingStrategy.cpp
+//  
+//
+//  Created by Nicolas Charles Herbert Broeking on 4/22/14.
+//
+//
+
+#include "EventMarshlingStrategy.hpp"
+
+
+int EventMarshlingStrategy::read(const unsigned char* io, size_t len, Event*& into)
+{
+    
+}
+size_t EventMarshlingStrategy::bytesNeeded( const Event& val)
+{
+    
+}
+void EventMarshlingStrategy::write(unsigned char* out, size_t len, const Event& val)
+{
+    
+}
